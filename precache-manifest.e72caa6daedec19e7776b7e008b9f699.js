a9a00f470bbdc9eb48f8e7cf7a8ed3649c6509ac
Sebastian Stryczek <sebastian.stryczek@hotmail.com>
2020-06-28 20:11:51 +0200

    Updates

---
+++ precache-manifest.e72caa6daedec19e7776b7e008b9f699.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2ed94795716dbb14360",
+    "url": "/react-app/static/css/main.161e29bc.chunk.css"
+  },
+  {
+    "revision": "e2ed94795716dbb14360",
+    "url": "/react-app/static/js/main.b045a22a.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/react-app/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-app/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6407c10c5d07f53808e690e5d562f6c9",
+    "url": "/react-app/index.html"
+  }
+];
